4c858d0a2897efab55bff94527f5f3c4025748c7
Prasongsuk Anuwong <japman@Prasongsuks-MacBook-Pro-2.local>
2026-06-24 17:45:17 +0700

    Add external_id to candidates for ECT UUID mapping

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -5,4 +5,5 @@ class Candidate < ApplicationRecord
   validates :number, presence: true, uniqueness: { scope: :election_id },
                      numericality: { only_integer: true, greater_than: 0 }
   validates :name, :color, presence: true
+  validates :external_id, uniqueness: true, allow_nil: true
 end
